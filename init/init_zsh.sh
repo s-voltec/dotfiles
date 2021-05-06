247a16dffc75df36d67fd05faf680120217e654f
熊野修平 <s@kumanoshuuheinoMacBook-Pro.local>
2021-05-06 19:19:49 +0900

    zplugのインストール処理を最新版に更新

--- init/init_zsh.sh
+++ init/init_zsh.sh
@@ -1,4 +1,4 @@
 if type zsh > /dev/null 2>&1; then
-    curl -sL zplug.sh/installer | zsh
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 fi
 
